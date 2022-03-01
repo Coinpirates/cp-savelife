eee9e8467f57b1c1ce7f4c44acfb76a4381c9e4c
Rost <rosman12@gmail.com>
2022-03-01 14:22:30 +0200

    about finish

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,17 +7,31 @@ import { HelperService } from '../helper.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit, AfterViewInit {
-
   constructor(private helperService: HelperService) { }
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit(): void {
+    const $ = (window as any).$;
     let hash = location.hash.split('-')[1];
     if(hash){
         this.helperService.scrollTo(hash);
     }
+    $('.about-header-right ul li')
+        .mouseenter(function() {
+          const index = $(this).index('.about-header-right ul li') + 1;
+          $(this).find('span').addClass('about-link-active');
+          $(this).append(`
+          <div id="aboutVideo">
+            <img src="assets/images/about/${index}.gif">
+          </div>
+          `);
+        })
+        .mouseleave(function() {
+          $(this).find('span').removeClass('about-link-active');
+          $('#aboutVideo').remove();
+        });
   }
 
   arts = [
